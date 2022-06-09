653ea10cd211245ea042534c85093c69e1a4391a
aaqib90 <mailmeaaqib@gmail.com>
2022-06-09 11:08:38 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -8,7 +8,7 @@ const path_1 = __importDefault(require("path"));
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 // const port: number = 3001
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 /**
  * parse  json from request body
